388d758f32ce05d59022743514d184b86c9b40f1
Josef Lange <josef.d.lange@gmail.com>
2024-09-02 18:27:21 -0500

    Font tweaks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,43 @@
-import type { Metadata } from "next";
-import { Raleway } from "next/font/google";
+import type {Metadata} from "next";
+import {Raleway} from "next/font/google";
+import localFont from 'next/font/local'
 import "./globals.css";
 import classNames from "classnames";
 
-const raleway = Raleway({ subsets: ["latin"] });
+const raleway = Raleway({subsets: ["latin"]});
+const gilroy = localFont({
+    src: [
+        {path: "fonts/Gilroy-ExtraBold.otf", weight: "800", style: "normal"},
+        {path: "fonts/Gilroy-Light.otf", weight: "300", style: "normal"},
+    ],
+    variable: '--font-gilroy',
+});
 
 export const metadata: Metadata = {
-  title: "Joey & Nikki Are Getting Married!",
-  description: "Made with <3 by Joey & Nikki",
+    title: "Joey & Nikki Are Getting Married!",
+    description: "Made with <3 by Joey & Nikki",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={styles.body}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body className={styles.body}>{children}</body>
+        </html>
+    );
 }
 
 
 const styles = {
-  body: classNames(
-      raleway.className,
-      "min-h-screen",
-      "bg-peacock",
-      "text-jadeite-100",
-      "grid grid-cols-12",
-  )
+    body: classNames(
+        gilroy.variable,
+        raleway.className,
+        "min-h-screen",
+        "bg-peacock",
+        "text-jadeite-100",
+        "grid grid-cols-12",
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ export default function Home() {
     return (
         <>
             <div className="col-start-1 col-end-13 md:col-start-2 md:col-end-6 py-24 h-full">
-                <h1 className="text-[8rem] font-extrabold leading-none">
+                <h1 className="text-[9rem] font-extrabold leading-none font-gilroy">
                     it&apos;s time to party
                 </h1>
             </div>
             <div className="col-start-1 col-end-13 md:col-start-7 md:col-end-12 self-end text-right py-24 space-y-8">
-                <p className="text-2xl font-semibold leading-none">
+                <p className="text-2xl font-medium leading-none">
                     Joey and Nikki are getting married!
                 </p>
                 <p className="text-md font-medium leading-none">
