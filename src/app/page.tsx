4cf6d13b41d0a2b679c7d1814ab49dad1f4e0c31
Josef Lange <josef.d.lange@gmail.com>
2025-01-01 13:37:33 -0600

    Add a few more detail blocks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,9 +40,11 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-                <Image src={"/joeyandnikki.jpg"} alt="Joey and Nikki" width={"1024"} height={"1024"} className="w-full md:w-1/3 rounded-full -mb-[50%] md:-mb-48" />
+                <Image src={"/joeyandnikki.jpg"} alt="Joey and Nikki" width={"1024"} height={"1024"}
+                       className="w-full md:w-1/3 rounded-full -mb-[50%] md:-mb-48"/>
             </div>
-            <div className="flex flex-col md:flex-row md:flex-wrap gap-12 md:gap-20 p-8 max-sm:pt-[45vw] md:p-12 lg:p-24">
+            <div
+                className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 md:max-w-[80%] gap-12 md:gap-16 p-8 max-sm:pt-[45vw] md:p-12 lg:p-24">
                 <InfoBlock title={"Where"}>
                     <p className="text-md font-medium leading-none">
                         The Camp Pow Wow Lodge<br/> at Menomonee Park
@@ -50,6 +52,10 @@ export default function Home() {
                     <p className="text-md font-medium leading-none text-peacock-800">
                         W220 N7884 Town Line Road,<br/>
                         Menomonee Falls, WI 53051
+                    </p>
+                    <p className="text-sm font-medium leading-none text-peacock-700 hover:underline">
+                        <a href={"https://www.google.com/maps/dir//Caml+Pow+Wow+Lodge,+Menomonee+Falls,+WI+53051/@43.1619774,-88.2592087,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x88045560e3a41739:0x2cef929157f5258d!2m2!1d-88.176808!2d43.162007"}
+                           target={"_blank"}>Get directions</a>
                     </p>
                 </InfoBlock>
                 <InfoBlock title={"When"}>
@@ -77,6 +83,29 @@ export default function Home() {
                 <InfoBlock title={"Attire"}>
                     <p className="text-md font-medium leading-none">
                         Semi-formal&mdash;dress boldly and colorfully!
+                    </p>
+                </InfoBlock>
+                <InfoBlock title={"Parking"}>
+                    <p className="text-md font-medium leading-none">
+                        Parking is available inside the park. <span className="italic">Please provide your license plate number</span> so we can cover your parking fee.
+                    </p>
+                </InfoBlock>
+                <InfoBlock title={"Accommodations"}>
+                    <p className="text-md font-medium leading-none">
+                        More information will be available soon.
+                    </p>
+                </InfoBlock>
+                <InfoBlock title={"Gifts"}>
+                    <p className="text-md font-medium leading-none">
+                        Your presence is the greatest gift we could ask for! <span className={"italic"}>Seriously! We mean it!</span>
+                    </p>
+                    <p className="text-md font-medium leading-none">
+                        If you still feel inclined to give, we would be grateful for a contribution to our <a href="https://www.honeyfund.com/site/joeyandnikki" target="_blank" className="text-peacock-700 hover:underline">honeymoon fund</a>.
+                    </p>
+                </InfoBlock>
+                <InfoBlock title={"Kids"}>
+                    <p className="text-md font-medium leading-none">
+                        We want your kids to have a great time, too! Please let us know if you plan to bring children so we can make sure there are activities for them.
                     </p>
                 </InfoBlock>
             </div>
